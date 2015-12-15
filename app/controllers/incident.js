133d72be16921fe407d193f39b8c3fefdf00c56e
Amir Sahib <amir@fifthtribe.com>
2015-12-14 20:10:08 -0500

    Fixed incident report isusues. Starting Map with leaflet.js

--- app/controllers/incident.js
+++ app/controllers/incident.js
@@ -1,13 +1,16 @@
-app.controller("IncidentCtrl",['$scope', '$firebaseObject', function($scope, $firebaseArray) {
+app.controller("IncidentCtrl",['$scope', '$firebaseArray', function($scope, $firebaseArray) {
   var ref = new Firebase("https://islamophobia-tracker.firebaseio.com/incidents");
 
   $scope.incidents = $firebaseArray(ref);
 
+  $scope.incidentSuccess = null;
+
   $scope.addIncident = function(incident){
+
     $scope.incidents.$add(incident).then(function(ref) {
-      var id = ref.key();
-      console.log("added record with id " + id);
-      list.$indexFor(id); // returns location in the array
+      $scope.incidentSuccess = "Sucessfully submitted the incident."
+      $scope.incident = {};
     });
   }
+
 }]);
